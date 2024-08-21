04a7b7c7eb9b8d01a0f187cd8204f2d623e8c154
Chaudhary0501 <chhayachaudhary1212@gmail.com>
2024-08-21 19:25:32 +0530

    more changes

--- models/user.js
+++ models/user.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-//USERNAME & PASSWORD are already defined by default by "passport-local-mongoose"
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-    }
+  email: {
+    type: String,
+    required: true,
+  },
 });
 
 userSchema.plugin(passportLocalMongoose);
-
 const User = mongoose.model("User", userSchema);
 module.exports = User;
